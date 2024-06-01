cc9d77fbed2df3f66cc0b943da3b8e9e0fedb8ca
danik-progger <lyatorovskiy@gmail.com>
2024-06-01 20:19:28 +0300

    Added enums and file read

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,57 @@
+import fs from "fs";
+
+// ----- ITERATOR -----
 const arr: number[] = [1, 2, 3].map((x) => x + 1);
 console.log(arr);
+
+// ----- READ FILE ------
+fs.readFileSync("src/lines.txt")
+    .toString()
+    .split("\n")
+    .filter((__, i) => i % 2 === 0)
+    .filter((__, i) => i > 1 && i < 4)
+    .forEach((line) => console.log(line));
+
+// ----- ENUM -----
+enum Color {
+    Red,
+    Green,
+    Blue,
+    Yellow
+}
+
+function printColor(color: Color) {
+    switch (color) {
+        case Color.Red:
+            console.log("red");
+            break;
+        case Color.Green:
+            console.log("green");
+            break;
+        case Color.Blue:
+            console.log("blue");
+    }
+}
+
+printColor(Color.Red);
+
+type Man = {
+    age: number,
+    name: string
+}
+
+type Item = number | string | Man;
+
+function append(items: Item[]) {
+    items.push("hello fem!");
+}
+
+const items: Item[] = [];
+console.log(items);
+append(items);
+console.log(items);
+
+const numbers: number[] = [1, 2, 3];
+console.log(numbers);
+append(numbers);
+console.log(numbers);
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-
+    // ----- ITERATOR -----
     let data = vec![1, 2, 3];
-    let mut arr = data
-        .iter()
-        .map(|x| x + 1);
+    let mut arr = data.iter().map(|x| x + 1);
 
     let mut new_vec: Vec<i32> = vec![];
 
@@ -12,4 +10,77 @@ fn main() {
     }
 
     println!("{:?}", new_vec);
+
+    // ----- READ FILE -----
+    let file = std::fs::read_to_string("src/lines.txt").unwrap();
+
+    file.lines()
+        .enumerate()
+        .filter(|(idx, _)| idx % 2 == 0)
+        .skip(2)
+        .take(2)
+        .for_each(|(_, line)| {
+            println!("{}", line);
+        });
+
+    // ----- ENUM -----
+    print_color(Color::Green);
+    let color = Color::Blue;
+    println!("{:?}", color.is_green());
+
+    let mut items: Vec<Item> = vec![];
+    append(&mut items);
+
+    // let mut items: Vec<usize> = vec![1, 2, 3];
+    // items.push(append(&mut items); -- error
+}
+
+// ----- ENUM -----
+enum Color {
+    Red,
+    Green,
+    Blue,
+    Yellow,
+}
+
+impl Color {
+    fn is_green(self) -> bool {
+        if let Color::Green = self {
+            return true;
+        }
+        return false;
+    }
+
+    fn is_green_part(&self) -> bool {
+        match self {
+            Color::Green => false,
+            Color::Blue => true,
+            Color::Yellow => true,
+            Color::Red => false,
+        }
+    }
+}
+
+fn print_color(color: Color) {
+    match color {
+        Color::Red => println!("red"),
+        Color::Green => println!("green"),
+        Color::Blue => println!("blue"),
+        Color::Yellow => println!("yellow"),
+    }
+}
+
+struct Man {
+    age: usize,
+    name: String,
+}
+
+enum Item {
+    Number(usize),
+    String(String),
+    Man(Man),
+}
+
+fn append(items: &mut Vec<Item>) {
+    items.push(Item::String("hello fem!".to_string()));
 }
